b10ba508503db84522d7e1facd3c4db0edd32d22
CTY <2664500451>
2018-10-15 09:52:41 +0800

    基础代码

---
+++ sql/init.sql
@@ -0,0 +1,19 @@
+
+-- INSERT INTO constant VALUES(1,'company','昭通日报社')
+
+insert  into permission (id,mod_code,mod_name,parent_mode_name,prem_state) values (1,'SYSTEM_ADMIN_USER','员工管理','系统管理','1');
+insert  into permission (id,mod_code,mod_name,parent_mode_name,prem_state) values (2,'SYSTEM_ADMIN_USER_ADD','员工添加','系统管理','1');
+insert  into permission (id,mod_code,mod_name,parent_mode_name,prem_state) values (3,'SYSTEM_ADMIN_USER_DEL','员工编辑','系统管理','1');
+insert  into permission (id,mod_code,mod_name,parent_mode_name,prem_state) values (4,'SYSTEM_ADMIN_USER_EDIT','员工删除','系统管理','1');
+insert  into permission (id,mod_code,mod_name,parent_mode_name,prem_state) values (5,'SYSTEM_ADMIN_ROLE','角色管理','系统管理','1');
+insert  into permission (id,mod_code,mod_name,parent_mode_name,prem_state) values (6,'SYSTEM_ADMIN_ROLE_ADD','角色添加','系统管理','1');
+insert  into permission (id,mod_code,mod_name,parent_mode_name,prem_state) values (7,'SYSTEM_ADMIN_ROLE_DEL','角色删除','系统管理','1');
+insert  into permission (id,mod_code,mod_name,parent_mode_name,prem_state) values (8,'SYSTEM_ADMIN_ROLE_EDIT','角色编辑','系统管理','1');
+
+
+-- insert  into permission (id,mod_code,mod_name,parent_mode_name,prem_state) values (9,'USER_TASK_MAN','员工任务管理','任务管理','1');
+
+
+
+
+insert  into permission (id,mod_code,mod_name,parent_mode_name,prem_state) values (10,'WORK_LOG_SHOW','工作日志','工作管理','1');
